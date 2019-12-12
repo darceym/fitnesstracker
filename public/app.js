95029806778997eb8523c9e4684fe44663ce1a9c
darceym <darcey.meus@gmail.com>
2019-12-11 20:52:16 -0500

    ajax calls for each of the submit buttons are working and saving data to the console

--- public/app.js
+++ public/app.js
@@ -1,2 +1,38 @@
 // click events for the two buttons
-$("#addbook").on("click", function() {
+$("#submitNew").on("click", function() {
+    $.ajax({
+      type: "POST",
+      url: "/submit",
+      dataType: "json",
+      data: {
+        new: $("#new").val(),
+        created: Date.now()
+      }
+    })
+      .then(function(data) {
+        console.log(data);
+        getNewWorkout();
+        $("#new").val("");
+      }
+      );
+    return false;
+  });
+
+  $("#submitPrevious").on("click", function() {
+    $.ajax({
+      type: "POST",
+      url: "/submit",
+      dataType: "json",
+      data: {
+        previous: $("#previous").val(),
+        created: Date.now()
+      }
+    })
+      .then(function(data) {
+        console.log(data);
+        getPreviousWorkout();
+        $("#previous").val("");
+      }
+      );
+    return false;
+  });
